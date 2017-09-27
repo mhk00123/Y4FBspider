77bdde15cb7a7ef4652fb925eb073038e307de80
maydayXi <onepiece762@gmail.com>
2017-09-27 16:46:59 +0800

    V3.0
    Add Database Read

--- DBService.py
+++ DBService.py
@@ -9,22 +9,50 @@ import sqlite3
 
 class DBService:
     
-    def insertData(self,id_lst,lat_lst,lon_lst,note_lst,pm25_lst,pm10_lst,t_lst,h_lst):
+    def createSiteData(self,id_lst,lat_lst,lon_lst,note_lst,):
         self.id_lst = id_lst
         self.lat_lst = lat_lst
         self.lon_lst = lon_lst
         self.note_lst = note_lst
-        self.pm25_lst = pm25_lst
-        self.pm10_lst = pm10_lst
-        self.t_lst = t_lst
-        self.h_lst = h_lst
         connection = sqlite3.connect('PM25.sqlite')
         
         for i in range(0,len(id_lst)):
-            sqlStr="insert into SiteInfo values('{}',{},{},'{}',{},{},{},{})".format(
-                    self.id_lst[i],self.lat_lst[i],self.lon_lst[i],self.note_lst[i],
-                    self.pm25_lst[i],self.pm10_lst[i],self.t_lst[i],self.h_lst[i])
+            sqlStr="insert into SiteInfo values('{}',{},{},'{}')".format(
+                    self.id_lst[i],self.lat_lst[i],self.lon_lst[i],self.note_lst[i],)
             connection.execute(sqlStr)
         connection.commit()
         connection.close()
+        
+    def createAirData(self,timeStr,id_lst,pm25_lst,pm10_lst,t_lst,h_lst):
+        self.timeStr = timeStr
+        connection = sqlite3.connect('PM25.sqlite')
+        sqlStr = """CREATE TABLE AirInfo_{} 
+                    (stId VARCHAR NOT NULL,
+                    PM25 INTEGER,
+                    PM10 INTEGER,
+                    Temperature FLOAT,
+                    Humidity FLOAT,
+                    PRIMARY KEY(stId),
+                    FOREIGN KEY(stId) REFERENCES SiteInfo(stId))""".format(self.timeStr)
+        connection.execute(sqlStr)
+        connection.commit()
+        
+        for i in range(0,len(id_lst)):
+            sqlStr = "INSERT INTO AirInfo_{} VALUES('{}',{},{},{},{})".format(
+                    self.timeStr,id_lst[i],pm25_lst[i],pm10_lst[i],t_lst[i],h_lst[i])
+            connection.execute(sqlStr)
+        connection.commit()
+        connection.close()
+    
+    def ReadAirData(self,timeStr):
+        
+        connection = sqlite3.connect('PM25.sqlite')
+        sqlStr = "select * from AirInfo_{}".format(timeStr)
+        cursor = connection.cursor()
+        cursor.execute(sqlStr)
+        data = cursor.fetchall()
+        
+        return data
+        
+        
         
--- main.py
+++ main.py
@@ -6,14 +6,13 @@ Created on Sun Sep 24 17:06:34 2017
 """
 #==============================import package==============================#
 
-from excelService import Service
-from DBService import DBService
-from bs4 import BeautifulSoup as bs
-import requests
+from excelService import Service        #excel class
+from DBService import DBService         #DB class
+from bs4 import BeautifulSoup as bs     #html parse package
+import requests,time                    #html package , time package
 
 #=================================Method===================================#
 def urlService():
-    
     #url handle
     for Id in all_id_lst:
         for sensor in sensor_lst:
@@ -25,7 +24,7 @@ def urlService():
             
             #remove invalid id
             if str(parse.find('title')).find('錯誤') != -1:
-                continue
+                error_id_lst.append(Id)
             else:
                 url_lst.append(url)
                 id_lst.append(Id)
@@ -64,7 +63,7 @@ def getAirValue():
 
 #==============================Main Programe===============================#
 
-#from excel get all cols data 
+#get all cols data from excel 
 excel = Service()
 all_id_lst = excel.getId()      #stId       
 all_lat_lst = excel.getLat()    #stLatitude
@@ -79,19 +78,28 @@ url_lst = []                    #useful url list
 lat_lst = []                    #useful latitude list
 lon_lst = []                    #useful longitude list
 note_lst = []                   #useful note list
+error_id_lst = []                  #useful error list
 
 #Get all useful info
 urlService()
 getUsefulValue()
 
-#Air lists
+#Air value lists
 pm25_lst = []                   #pm2.5 lsit
 pm10_lst = []                   #pm10 list
 t_lst = []                      #temperatuer lsit
 h_lst = []                      #humidity list
 getAirValue()                   #get all air value
 
+#database instance
 db=DBService()
-db.insertData(id_lst,lat_lst,lon_lst,note_lst,pm25_lst,pm10_lst,t_lst,h_lst)
+#db.createSiteData(id_lst,lat_lst,lon_lst,note_lst)
 
+timeStr = time.strftime('%Y_%m_%d_%H_%M')                           #get current time
+db.createAirData(timeStr,id_lst,pm25_lst,pm10_lst,t_lst,h_lst)      #Create AriInfo table
+data = db.ReadAirData(timeStr)
+
+for item in data:
+    print(item[0],item[1],item[2],item[3],item[4])
 #excel.outputExcel(id_lst,note_lst,pm25_lst,pm10_lst,t_lst,h_lst)
+
